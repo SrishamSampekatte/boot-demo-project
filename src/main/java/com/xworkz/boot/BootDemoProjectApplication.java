7b8649c91767c8fc0e743f64f1acccaa125c0ce9
SrishamSampekatte <srishamsm.xworkz@gmail.com>
2024-07-04 20:13:01 +0530

    First Commit

---
+++ src/main/java/com/xworkz/boot/BootDemoProjectApplication.java
@@ -0,0 +1,22 @@
+package com.xworkz.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class BootDemoProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootDemoProjectApplication.class, args);
+		System.out.println("Hello");
+	}
+
+	@GetMapping("/")
+	public String index() {
+		return "Greetings from Spring Boot!";
+	}
+
+}
